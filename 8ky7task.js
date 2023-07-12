380c77fdc6f1ee1f6d0983db5c762a986a8dcbe3
Denys Pochapskyi <94985901+dancqre@users.noreply.github.com>
2023-07-12 19:13:03 +0300

    Fake Binary

---
+++ 8ky7task.js
@@ -0,0 +1,12 @@
+function fakeBin(x){
+ let result = '';
+  
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] < '5') {
+      result += '0';
+    } else {
+      result += '1';
+    }
+  }
+  return result;
+}
