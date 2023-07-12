e6e142e88d2aa1570e582d9d33f0d893a668c559
Denys Pochapskyi <94985901+dancqre@users.noreply.github.com>
2023-07-12 17:27:45 +0300

    Basic Mathematical Operations

---
+++ 8ky4task.js
@@ -0,0 +1,8 @@
+function basicOp(operation, value1, value2)
+{
+  // Code
+  if(operation == "+") return value1 + value2;
+  else if(operation == "-") return value1 - value2;
+  else if(operation == "*") return value1 * value2;
+  else if(operation == "/") return value1 / value2;
+}
