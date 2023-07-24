1c8c08cd43e03f717dea9cc8fd890a2e1a36ca07
Denys Pochapskyi <94985901+dancqre@users.noreply.github.com>
2023-07-25 02:21:47 +0300

    Categorize New Member

---
+++ 7ky.task4.js
@@ -0,0 +1,15 @@
+function openOrSenior(data){
+  // ...
+  
+  const categories  = []; 
+  
+  for (const [years, handicap] of data){
+    if ( years >= 55 && handicap >= 7){
+      categories.push('Senior');
+    }else categories.push('Open');
+  }
+  
+  
+  return categories;
+
+}
