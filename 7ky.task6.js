13aac60bdc6f170ea8e9a9521e958612db093546
Denys Pochapskyi <94985901+dancqre@users.noreply.github.com>
2023-07-25 02:59:13 +0300

    Multiples

---
+++ 7ky.task6.js
@@ -0,0 +1,9 @@
+function multiple(x) {
+  // Good Luck
+  if(x % 3 == 0 && x % 5 == 0){
+    return "BangBoom" 
+  } 
+  else if(x % 3 == 0) {return "Bang"}
+  else if(x % 5 == 0) {return "Boom"}
+  else return "Miss";
+}
